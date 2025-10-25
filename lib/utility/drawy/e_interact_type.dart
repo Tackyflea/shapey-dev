9d2be49cac14cdbd5cc3f25fbca0048266d6c8c7
Lucky Dee <luckydeto@gmail.com>
2025-10-25 14:53:09 -0400

    added right click menu

--- lib/utility/drawy/e_interact_type.dart
+++ lib/utility/drawy/e_interact_type.dart
@@ -1,2 +1,5 @@
 // for mouse / touch interaction simplicity
 enum DrawyInteract { start, move, end }
+
+// for mouse interaction over keyframes
+enum KeyframeInteract { over, selected, none, menuOpen }
--- lib/widgets/timeline/timeline_keys_widget.dart
+++ lib/widgets/timeline/timeline_keys_widget.dart
@@ -1,33 +1,157 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_context_menu/flutter_context_menu.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:shapey/app_state/file_model.dart';
+import 'package:shapey/utility/drawy/e_interact_type.dart';
+import 'package:shapey/widgets/timeline/layer_name_widget.dart';
+import 'package:shapey/widgets/timeline/timeline_keys_context_menu.dart';
 
 // generic key bg
-class KeyVisual extends StatelessWidget {
+class KeyVisual extends StatefulWidget {
   final int frameNumber;
   final double fps;
   const KeyVisual({super.key, required this.frameNumber, required this.fps});
 
   @override
+  State<KeyVisual> createState() => _KeyVisualState();
+}
+
+double borderSize = 1;
+
+class _KeyVisualState extends State<KeyVisual> {
+  // passable back to the file setting .. somehow
+  bool keyed = false;
+
+  // to indicate current interation if any over keyframe
+  KeyframeInteract keyInteraction = KeyframeInteract.none;
+  @override
   Widget build(BuildContext context) {
+    // the right click menu. It's made here so it gets context on what to enable
+    final rc_menu_keyframe = <ContextMenuEntry>[
+      MenuItem(
+        label: 'Add Keyframe',
+        icon: Icons.add,
+        enabled: !keyed,
+        value: "add",
+      ),
+      MenuItem(
+        label: 'Remove Keyframe',
+        icon: Icons.remove,
+        enabled: keyed,
+        value: "remove",
+      ),
+    ];
+
     final ColorScheme colorScheme = Theme.of(context).colorScheme;
-    final boxBorder = Border.all(
+    Color outputColor;
+    Border outputBorder = Border.all(
       color: colorScheme.surfaceContainerHighest,
       width: 1.0,
       style: BorderStyle.solid,
       strokeAlign: BorderSide.strokeAlignCenter,
     );
-    final Color keyColor;
-    final bool isWholeSecond = frameNumber.toDouble() % fps == 0;
+
+    Border boxBorderKeyed = Border.all(
+      color: colorScheme.tertiary,
+      width: 1.0,
+      style: BorderStyle.solid,
+      strokeAlign: BorderSide.strokeAlignInside,
+    );
+    Border boxBorderRollOvered = Border.all(
+      color: colorScheme.primary,
+      width: 1.0,
+      style: BorderStyle.solid,
+      strokeAlign: BorderSide.strokeAlignCenter,
+    );
+
+    Border boxBorderRollOverKeyed = Border.all(
+      color: colorScheme.primary,
+      width: 2.0,
+      style: BorderStyle.solid,
+      strokeAlign: BorderSide.strokeAlignCenter,
+    );
+
+    Color defaultColor;
+    final bool isWholeSecond = widget.frameNumber.toDouble() % widget.fps == 0;
     //custom color for second
     if (isWholeSecond) {
-      keyColor = colorScheme.surfaceContainerHighest;
+      defaultColor = colorScheme.surfaceContainerHighest;
     } else {
-      keyColor = colorScheme.onSecondary;
+      defaultColor = colorScheme.onSecondary;
     }
-    return Container(
-      alignment: Alignment.center,
-      decoration: BoxDecoration(color: keyColor, border: boxBorder),
+    outputColor = defaultColor;
+    // default keyframed color
+    if (keyed) {
+      outputColor = colorScheme.tertiaryFixed;
+      outputBorder = boxBorderKeyed;
+    }
+    // roll over colors
+    if (keyInteraction == KeyframeInteract.over ||
+        keyInteraction == KeyframeInteract.menuOpen) {
+      // if its already keyed
+      if (keyed) {
+        outputColor = colorScheme.tertiaryFixed.withAlpha(130);
+        outputBorder = boxBorderRollOverKeyed;
+      } else {
+        // default rollover
+        outputColor = colorScheme.inversePrimary;
+        outputBorder = boxBorderRollOvered;
+      }
+    }
+    return InkWell(
+      onSecondaryTapDown: (e) async {
+        // immediately mark menu is open
+        setState(() => (keyInteraction = KeyframeInteract.menuOpen));
+        // right click
+        final menu = ContextMenu(
+          entries: rc_menu_keyframe,
+          position: e.globalPosition,
+          padding: const EdgeInsets.all(8.0),
+        );
+
+        //WAIT until user picks something
+        final selectedValue = await ContextMenu(
+          entries: rc_menu_keyframe,
+          position: e.globalPosition,
+          padding: const EdgeInsets.all(8.0),
+        ).show(context);
+
+        // cancel operation if the whole thing gets dropped.
+        if (selectedValue == null) {
+          setState(() => (keyInteraction = KeyframeInteract.none));
+        }
+
+        // act on decision , if any
+        if (selectedValue == "add") {
+          setState(() {
+            keyed = true;
+            keyInteraction = KeyframeInteract.none;
+          });
+        }
+        if (selectedValue == "remove") {
+          setState(() {
+            keyed = false;
+            keyInteraction = KeyframeInteract.none;
+          });
+        }
+      },
+      onHover: (value) {
+        // print('roll over $value');
+        if (value) {
+          // roll over
+          setState(() => keyInteraction = KeyframeInteract.over);
+        } else {
+          // roll out
+          if (keyInteraction != KeyframeInteract.menuOpen) {
+            setState(() => (keyInteraction = KeyframeInteract.none));
+          }
+        }
+      },
+
+      child: Container(
+        alignment: Alignment.center,
+        decoration: BoxDecoration(color: outputColor, border: outputBorder),
+      ),
     );
   }
 }
